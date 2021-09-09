5499756f279834234a1908bba2b7eabde8c80a0c
Sai Shashank Vanga <svanga@unc.edu>
2021-09-08 21:01:55 -0400

    practice code 09/08

---
+++ lessons/practice2.py
@@ -0,0 +1,13 @@
+choice: int = int(input("Enter a number: "))
+
+if choice > 50:
+    if choice < 25:
+        print("A")
+    else:
+        print("B")
+else:
+    if choice > 75:
+        print("C")
+    else:
+        print("D")
+        
