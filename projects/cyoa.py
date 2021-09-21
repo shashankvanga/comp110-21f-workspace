38c73e6a7a041cff20dabb3de5b69292e438f8bc
Sai Shashank Vanga <svanga@unc.edu>
2021-09-20 23:10:26 -0400

    project0 final

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -32,6 +32,7 @@ def incorrect_door(correct : int, choice : int) -> int:
 
 
 def rules(points: int):
+    """returns the rules and strategies which can be used to win the game"""
     global strategy_unlocks, description
     rules_1 = int(input(
 f'''
@@ -47,6 +48,7 @@ The Monty Hall problem is a classic game theory problem, where the player has to
 The host knows which door has the prize behind it, and will choose to open an incorrect door (it cannot be the one the player chose).
 The player will then be given the option to either stay with their choice or switch to the other closed door.
 Their final decision could be the only thing in their way of winning a grand prize!
+Start playing the game to earn one free point, but feel free to use the 'Strategy' option at the cost of one point!
 '''
         )
         points += 1
@@ -71,7 +73,7 @@ def simulate():
     """Simulates a run of Monty Hall"""
     global player, points, runs, wins
     runs += 1
-    choice = int(input(f"Okay, {player}! You have three doors - 1, 2 and 3. Choose your lucky door! "))
+    choice = int(input(f"Okay, {player}! You have three doors: 1, 2 and 3. Choose your lucky door! "))
     points += 1
     jackpot = correct_door()
 
@@ -99,12 +101,13 @@ def simulate():
     if won:
         points += 2
         wins += 1
-        print(f"Congrats! {HAPPY_FACE} You win. The correct door was {jackpot}!")
+        print(f"Congrats! {HAPPY_FACE} You win! The correct door was {jackpot}!")
     else:
-        print(f"Oops! Tough luck. {SAD_FACE} The correct door was {jackpot}")
+        print(f"Oops! Tough luck! {SAD_FACE} The correct door was {jackpot}")
             
 
 def greet():
+    """greets the player as soon as the program is run"""
     global player
     print("Welcome to the Monty Hall Problem!")
     player = input("Enter your name to begin : ")
@@ -112,9 +115,10 @@ def greet():
 
 
 def main():
+    """main function that runs the whole game"""
     global points, runs, wins, strategy_unlocks, description
     greet()
-    print(f"Welcome to Monty Hall Problem {player} !")
+    print(f"Welcome to the Monty Hall Problem {player}!")
     points = 0
     runs = 0
     wins = 0
@@ -129,10 +133,11 @@ def main():
         choice = int(input())
         if choice == 1:
             simulate()
+            print(f"Well done {player}, you scored {points} points!")
         if choice == 2:
             points = rules(points)
-    print(f"Adios {player}! You played well and scored {points} points")
-    print("Here are a few more stats for you")
+    print(f"Adios {player}! You played well and scored {points} points!")
+    print("Here are a few more stats for you:")
     print(f"You played {runs} times and won {wins} times!")
     print(f"You unlocked the strategy {strategy_unlocks} times")
     print(f"You read the description {description} times!")
